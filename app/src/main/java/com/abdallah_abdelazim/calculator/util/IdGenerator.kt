72468eab0b56ba1a3d020ac15b6dbfc1771cbdd2
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2021-09-25 02:20:46 +0200

    Make IdGenerator thread-safe

--- app/src/main/java/com/abdallah_abdelazim/calculator/util/IdGenerator.kt
+++ app/src/main/java/com/abdallah_abdelazim/calculator/util/IdGenerator.kt
@@ -2,8 +2,10 @@ package com.abdallah_abdelazim.calculator.util
 
 object IdGenerator {
 
+    @Volatile
     private var counter = 0
 
+    @Synchronized
     fun generateUniqueId(): Int {
         return counter++
     }
